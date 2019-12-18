482504a6fdd0de46bb6a7812b2de56fd9b5c04b0
Osman Akdağ <akdagosman45@gmail.com>
2019-12-18 21:05:56 +0300

    lamba ve firebase baglantisi yapildi

---
+++ Lamba_ve_firebase/Lamba_ve_firebase.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+// Set these to run example.
+#define FIREBASE_HOST "ev-otomasyon-f0c18.firebaseio.com"
+#define FIREBASE_AUTH "uK0BHzQJMsZSLXSKipLVM989vQbP9jxGlFJWdtnc"
+#define WIFI_SSID "huawei"
+#define WIFI_PASSWORD "osman123"
+void setup() {
+Serial.begin(9600);
+pinMode(D1, OUTPUT);
+// connect to wifi.
+WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+Serial.print("connecting");
+while (WiFi.status() != WL_CONNECTED) {
+Serial.print(".");
+delay(500);
+}
+Serial.println();
+Serial.print("connected: ");
+Serial.println(WiFi.localIP());
+Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+Firebase.set("lamba", 0);
+}
+int n;
+void loop() {
+// get value
+n = Firebase.getInt("lamba");
+// handle error
+if (n==1) {
+Serial.println("LED ON");
+digitalWrite(D1,HIGH);  
+return;
+delay(10);
+}
+else {
+Serial.println("LED OFF");
+digitalWrite(D1,LOW);  
+return;
+}
+}
