18d79af1fdd9067b10f0051f07e3ea6ed3a5abf2
osman4597 <osmanakdag45@hotmail.com>
2020-10-02 15:39:27 +0300

    added wifi scan

--- Project_home/Project_home.ino
+++ Project_home/Project_home.ino
@@ -1,49 +1,38 @@
-//Kutuphanelerin tanimi
-#include <ESP8266WiFi.h>                                                    
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>                                                 
 #include <FirebaseArduino.h>                                                
-#include <DHT.h>                                                            
+#include <DHT.h>  
 
-//Firebase anahtar ve veritabani adi
-#define FIREBASE_HOST "*** BURAYA FIREBASE VERITABANI ADI ***"                         
-#define FIREBASE_AUTH "*** BURAYA VERI TABANI BAĞLANTI ANAHTARI ***"            
+//80 portunda server çalışıyor
+WiFiServer server(80);
 
-//Wireless ssid ve sifre
-#define WIFI_SSID "*** BURAYA WIFI-SSID"                                             
-#define WIFI_PASSWORD "*** BURAYA WIFI SIFRE ***"                                    
+String header;
 
+String output5State = "off";
+String output4State = "off";
+
+const int output5 = 5;
+const int output4 = 4;
+
+#define FIREBASE_HOST "ev-otomasyon-f0c18.firebaseio.com"                         
+#define FIREBASE_AUTH "uK0BHzQJMsZSLXSKipLVM989vQbP9jxGlFJWdtnc"
 #define DHTPIN D4        //Sicaklik ve nem sensoru pin belirlenmesi                                                   
 #define DHTTYPE DHT22    //Sicaklik sensor tanimi                                               
 DHT dht(DHTPIN, DHTTYPE); 
 
-void setup() 
-{
+void setup() {
   Serial.begin(9600); //Serial haberlesmenin baslatilmasi
   pinMode(D1, OUTPUT); //led pinin tanimi
   pinMode(D2,OUTPUT); //fan ledinin tanimi
-  delay(1000);                
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD); //Wireless haberlesmenin baslatilmasi    
-  //Wireles haberlesmenin serial ekranda yazdirilmasi                            
-  Serial.print("Connecting to ");
-  Serial.print(WIFI_SSID);
-
-  //Wireless agina baglanana kadar denenmesi
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(500);
-  }
-
-  //Wireless aga baglanma durumu
-  Serial.println();
-  Serial.print("Connected to ");
-  Serial.println(WIFI_SSID);
-  Serial.print("IP Address is : ");
-  Serial.println(WiFi.localIP());
-  //Wireless aga baglanma durumunda firebase veritabanina verilen isim ve key ile baglanma                                
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);    
-  //Sicaklik sensorun calistirilmasi                        
-  dht.begin();                                                              
+  delay(1000); 
+ 
+  WiFiManager wifiManager;
+  //wifiManager.resetSettings(); //Cihaz içindeki wifi ayarlarını sıfırlamak için bu kodun 1 kez yazdırılması gerek
+  wifiManager.autoConnect("NodeMcu WiFi Ayar");
+  Serial.println("Connected.");
+  
+  server.begin();  
 }
-
 //Sicaklik ve nem degerlerinin veritabanina yazilmasi
 void sicakliknem(float t,float h)
 { 
@@ -101,14 +90,84 @@ void gaz(int deger)
 
 }
 
+void loop(){
+  //Wifi baglanmak için olusturulan html kodları
+  WiFiClient client = server.available();
 
-//NodeMcu'nun calismasi durumunda sensor fonksiyonlarinin cagirilmasi ve calistirilmasi
-//Cihaz her 2 saniyede bir veri aktarimi saglamaktadir.
-void loop() 
-{ 
+  if (client) {      //Yeni bir kullanıcı durumunda                
+    Serial.println("New Client.");          
+    String currentLine = "";                
+    while (client.connected()) {            
+      if (client.available()) {             
+        char c = client.read();             
+        Serial.write(c);                    
+        header += c;
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("GPIO 5 on");
+              output5State = "on";
+              digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("GPIO 5 off");
+              output5State = "off";
+              digitalWrite(output5, LOW);
+            } else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("GPIO 4 on");
+              output4State = "on";
+              digitalWrite(output4, HIGH);
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("GPIO 4 off");
+              output4State = "off";
+              digitalWrite(output4, LOW);
+            }
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #f7c933; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");      
+            client.println("<body><h1>NodeMcu Web Sunucu</h1>");
+            
+            client.println("<p>GPIO 5 - State " + output5State + "</p>");     
+            if (output5State=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+            client.println("<p>GPIO 4 - State " + output4State + "</p>");     
+            if (output4State=="off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+            client.println("</body></html>");  
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {  
+          currentLine += c;
+        }
+      }
+    }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  dht.begin(); 
   lamba(Firebase.getInt("lamba"));
   klima(Firebase.getInt("klima"));
   sicakliknem(dht.readTemperature(),dht.readHumidity());
   gaz(analogRead(A0));
-  delay(2000);                                     
+  delay(2000);   
 }
