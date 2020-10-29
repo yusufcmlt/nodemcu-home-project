627b3025b50068a62d2c4912d3309235d1ab3830
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-10-29 19:35:49 +0300

    assets object

---
+++ nodemcu-client/src/assets/assets.js
@@ -0,0 +1,18 @@
+const imageAsset = {
+  lamp:
+    "https://firebasestorage.googleapis.com/v0/b/ev-otomasyon-f0c18.appspot.com/o/assets%2Flamp.svg?alt=media&token=2201ef9d-8e21-49ce-92b2-4335db9ef0cb",
+  lampColored:
+    "https://firebasestorage.googleapis.com/v0/b/ev-otomasyon-f0c18.appspot.com/o/assets%2Flamp-colored.svg?alt=media&token=d53c620f-dc96-4028-a4e2-a337dd48bd0c",
+  fan:
+    "https://firebasestorage.googleapis.com/v0/b/ev-otomasyon-f0c18.appspot.com/o/assets%2Ffan.svg?alt=media&token=30ba68dc-1f59-47ac-abbf-859b050bf808",
+  fanColored:
+    "https://firebasestorage.googleapis.com/v0/b/ev-otomasyon-f0c18.appspot.com/o/assets%2Ffan-colored.svg?alt=media&token=2a158ffa-c172-457b-b230-ff939794cf03",
+  smoke:
+    "https://firebasestorage.googleapis.com/v0/b/ev-otomasyon-f0c18.appspot.com/o/assets%2Fcigarrete.svg?alt=media&token=ef3bee50-df29-4d03-935f-eee876618bf8",
+  temperature:
+    "https://firebasestorage.googleapis.com/v0/b/ev-otomasyon-f0c18.appspot.com/o/assets%2Fthermometer.svg?alt=media&token=97feefe2-8711-4dbf-9878-4649a81127c4",
+  humidity:
+    "https://firebasestorage.googleapis.com/v0/b/ev-otomasyon-f0c18.appspot.com/o/assets%2Fwater-drop.svg?alt=media&token=920e0c85-13ae-41e8-b682-9ebabd7c8e09",
+};
+
+export default imageAsset;
